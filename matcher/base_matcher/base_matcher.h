721183e528e761d743709d7bf8a927e304e0d51e
acai66 <1779864536@qq.com>
2024-03-05 13:02:52 +0800

    first commit

---
+++ matcher/base_matcher/base_matcher.h
@@ -0,0 +1,60 @@
+﻿#ifndef _BOYAN_AI_BASEMATCHER_H 
+#define _BOYAN_AI_BASEMATCHER_H 
+
+#include "spdlog/spdlog.h"
+#include "spdlog/async.h"
+#include "spdlog/sinks/rotating_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include <random>
+#include <chrono>
+#include "boyanmatcher.h"
+
+#include "omp.h"
+
+
+namespace boyan_template_matching
+{
+	class BaseMatcher : public Matcher
+	{
+	public:
+		BaseMatcher();
+
+		~BaseMatcher();
+
+		// 判断是否初始化成功
+		bool isInited();
+
+		virtual void drawResult(const cv::Mat& frame, std::vector<MatchResult> matchResults);
+
+		void setMetricsTime(const bool& enabled);
+
+		bool getMetricsTime();
+
+	protected:
+
+		// 初始化匹配器
+		bool initMatcher(const MatcherParam& param);
+
+		// 初始化完成标志位
+		bool initFinishedFlag_ = false;
+
+		// 匹配参数
+		MatcherParam matchParam_;
+
+		// 计时
+		std::chrono::steady_clock::time_point startTime_, endTime_;
+		std::chrono::duration<double> timeUse_;
+
+		// 评估时间
+		bool metricsTime_ = false;
+
+		std::shared_ptr<spdlog::logger> logger_;
+
+
+	private:
+
+
+	};
+}
+
+#endif
